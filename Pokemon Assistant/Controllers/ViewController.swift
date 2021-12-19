8e8648fc922d0da0395456e7c311d455ee1d49c2
Tim Bausch <baus8650@gmail.com>
2021-12-19 14:52:07 -0800

    Add text field consistency

--- Pokemon Assistant/Controllers/ViewController.swift
+++ Pokemon Assistant/Controllers/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationController?.navigationBar.prefersLargeTitles = true
         title = "Search"
         
     }
